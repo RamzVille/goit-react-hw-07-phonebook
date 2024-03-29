ec6439386321e01ac633ba11f4fe08e2443f4a9f
Ramz Ville <ramzville22@gmail.com>
2024-03-29 13:10:52 +0800

    hw7-phonebook-1st

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
 export const App = () => {
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={{ margin: '50px auto', paddingTop: '10px', width: '500px', backgroundColor: 'rgb(255, 255, 255)', borderRadius: ' 5px', boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2px 10px 0 rgba(0, 0, 0, 0.19)' }}>
+      <h1 style={{ marginLeft: '15px' }}>Phonebook</h1>
+      <ContactForm />
+      <h2 style={{ marginLeft: '25px' }}>Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
   );
 };
